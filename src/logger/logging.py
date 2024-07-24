0e0f6a2125f4d3da74306fb183dbc53f9d4b3774
Ayazulhaq Yousafzai <www.ayazkhan.com.21@gmail.com>
2024-07-24 09:38:21 -0700

    Changes have been made successfully

--- src/logger/logging.py
+++ src/logger/logging.py
@@ -2,13 +2,13 @@ import logging
 import os
 from datetime import datetime
 
-LOG_FILE=f"{datetime.now().strftime('%m_%d_%Y_%H_%M_%S')}.log"
+LOG_FILES=f"{datetime.now().strftime('%m_%d_%Y_%H_%M_%S')}.log"
 
 log_path=os.path.join(os.getcwd(),"logs")
 
 os.makedirs(log_path,exist_ok=True)
 
-LOG_FILEPATH=os.path.join(log_path,LOG_FILE)
+LOG_FILEPATH=os.path.join(log_path,LOG_FILES)
 
 
 logging.basicConfig(level=logging.INFO, 
@@ -16,6 +16,6 @@ logging.basicConfig(level=logging.INFO,
                     format="[%(asctime)s] %(lineno)d %(name)s - %(levelname)s - %(message)s"
                     
 )
-            #[2024-01-10 15:57:26,997] 6 root - INFO -  this my second tesgting
+         
 
 
